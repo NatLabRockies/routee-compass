ee10710bdf92660115204abec5561ae2e744846f
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2025-12-30 14:02:30 -0700

    validate lat/lon values

--- rust/routee-compass-omf/src/app/cli_bbox.rs
+++ rust/routee-compass-omf/src/app/cli_bbox.rs
@@ -14,22 +14,11 @@ pub fn parse_bbox(s: &str) -> Result<CliBoundingBox, String> {
         return Err(format!("expected format: xmin,xmax,ymin,ymax, got: {}", s));
     }
 
-    let xmin = parts[0]
-        .trim()
-        .parse::<f32>()
-        .map_err(|_| format!("invalid xmin: {}", parts[0]))?;
-    let xmax = parts[2]
-        .trim()
-        .parse::<f32>()
-        .map_err(|_| format!("invalid xmax: {}", parts[1]))?;
-    let ymin = parts[1]
-        .trim()
-        .parse::<f32>()
-        .map_err(|_| format!("invalid ymin: {}", parts[2]))?;
-    let ymax = parts[3]
-        .trim()
-        .parse::<f32>()
-        .map_err(|_| format!("invalid ymax: {}", parts[3]))?;
+    let xmin = parse_lon(parts[0])?;
+    let xmax = parse_lon(parts[1])?;
+
+    let ymin = parse_lat(parts[2])?;
+    let ymax = parse_lat(parts[3])?;
 
     Ok(CliBoundingBox {
         xmin,
@@ -42,5 +31,27 @@ pub fn parse_bbox(s: &str) -> Result<CliBoundingBox, String> {
 impl std::fmt::Display for CliBoundingBox {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{},{},{},{}", self.xmin, self.xmax, self.ymin, self.ymax)
+    }
+}
+
+fn parse_lat(lat: &str) -> Result<f32, String> {
+    parse_num(lat, -90.0, 90.0).map_err(|e| format!("invalid latitude: {e}"))
+}
+
+fn parse_lon(lat: &str) -> Result<f32, String> {
+    parse_num(lat, -180.0, 180.0).map_err(|e| format!("invalid longitude: {e}"))
+}
+
+fn parse_num(s: &str, min: f32, max: f32) -> Result<f32, String> {
+    let v = s
+        .trim()
+        .parse::<f32>()
+        .map_err(|_| format!("not a number: {s}"))?;
+    if v < min || max < v {
+        Err(format!(
+            "number '{v}' is not valid, must be in range [{min},{max}]"
+        ))
+    } else {
+        Ok(v)
     }
 }
