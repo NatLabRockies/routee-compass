e7f6b46b1212de262d83d5e47900f04f6339ef1a
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-01-13 14:07:32 -0700

    cargo clippy

--- rust/routee-compass-omf/src/graph/segment_ops.rs
+++ rust/routee-compass-omf/src/graph/segment_ops.rs
@@ -73,7 +73,7 @@ pub fn get_headings(
         rs.iter()
             .any(|r| r.access_type == SegmentAccessType::Denied)
     }) {
-        if valid_headings.len() > 0 {
+        if !valid_headings.is_empty() {
             let gers_id = &segment.id;
             let when_str = serde_json::to_string_pretty(&when.as_ref()).unwrap_or_default();
             log::debug!("segment {gers_id} with test_fwd={test_fwd}, test_bwd={test_bwd} overcame denial for query:\n{when_str}");
