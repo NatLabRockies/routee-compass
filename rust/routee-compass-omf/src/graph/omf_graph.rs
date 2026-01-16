5e3d55fdd129bd256a2cc1b5ee8c026fa5a983f3
Yamil Essus <Yamil.Essus@nrel.gov>
2026-01-16 14:21:07 -0500

    remove commented code

--- rust/routee-compass-omf/src/graph/omf_graph.rs
+++ rust/routee-compass-omf/src/graph/omf_graph.rs
@@ -274,26 +274,3 @@ impl OmfGraphVectorized {
         Ok(())
     }
 }
-
-// /// helper function to build a filewriter for writing either .csv.gz or
-// /// .txt.gz files for compass datasets while respecting the user's overwrite
-// /// preferences and properly formatting WKT outputs.
-// fn create_writer(
-//     directory: &Path,
-//     filename: &str,
-//     has_headers: bool,
-//     quote_style: QuoteStyle,
-//     overwrite: bool,
-// ) -> Option<csv::Writer<GzEncoder<File>>> {
-//     let filepath = directory.join(filename);
-//     if filepath.exists() && !overwrite {
-//         return None;
-//     }
-//     let file = File::create(filepath).unwrap();
-//     let buffer = GzEncoder::new(file, Compression::default());
-//     let writer = csv::WriterBuilder::new()
-//         .has_headers(has_headers)
-//         .quote_style(quote_style)
-//         .from_writer(buffer);
-//     Some(writer)
-// }
