65ff31b86f5449b42a1016435737fbe8aefb0c16
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-01-16 09:34:38 -0700

    already a vec

--- rust/routee-compass-omf/src/collection/record/geometry_wkb_codec.rs
+++ rust/routee-compass-omf/src/collection/record/geometry_wkb_codec.rs
@@ -31,7 +31,7 @@ where
 
     data.map(|v| {
         let bytes = match &v {
-            BytesOrString::Bytes(b) => b.clone().into_vec(),
+            BytesOrString::Bytes(b) => b.clone(),
             BytesOrString::String(s) => hex::decode(s).map_err(|e| {
                 serde::de::Error::custom(format!("failure converting hex wkb string to bytes: {e}"))
             })?,
