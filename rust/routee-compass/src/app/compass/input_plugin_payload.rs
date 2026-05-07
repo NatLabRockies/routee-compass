abac4e8356e034aca5763806791e1cc6d8e96ed9
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-05-07 16:51:41 -0600

    remove unused

--- rust/routee-compass/src/app/compass/input_plugin_payload.rs
+++ rust/routee-compass/src/app/compass/input_plugin_payload.rs
@@ -30,26 +30,6 @@ impl InputPluginPayload {
         }
     }
 
-    /// creates a new [InputPluginResult] with a new row, moving existing
-    /// runtimes data.
-    pub fn update_row(self, new_row: Value) -> Self {
-        Self {
-            row: new_row,
-            error: None,
-            runtimes: self.runtimes,
-        }
-    }
-
-    /// clones existing runtimes data and inserts a new row. used when
-    /// a plugin produces more than one row.
-    pub fn create_child_row(&self, new_row: Value) -> Self {
-        Self {
-            row: new_row,
-            error: None,
-            runtimes: self.runtimes.clone(),
-        }
-    }
-
     /// records a runtime for an input plugin. will generate proportional runtimes
     /// based on the number of result rows of the plugin.
     pub fn record_input_plugin_runtime(
