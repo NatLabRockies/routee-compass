c33478769f336b2167639db7221fd13f12955626
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-06-02 13:45:19 -0600

    store config in engine

--- rust/routee-compass-core/src/model/traversal/template/engine.rs
+++ rust/routee-compass-core/src/model/traversal/template/engine.rs
@@ -2,12 +2,14 @@ use super::TemplateConfig;
 
 use crate::model::traversal::TraversalModelError;
 
-pub struct TemplateEngine {}
+pub struct TemplateEngine {
+    config: TemplateConfig,
+}
 
 impl TryFrom<TemplateConfig> for TemplateEngine {
     type Error = TraversalModelError;
 
-    fn try_from(_config: TemplateConfig) -> Result<Self, Self::Error> {
-        todo!()
+    fn try_from(config: TemplateConfig) -> Result<Self, Self::Error> {
+        Ok(Self { config })
     }
 }
