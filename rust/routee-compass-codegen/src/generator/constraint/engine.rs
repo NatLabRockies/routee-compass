40e4465e6ec470270303b4e5e6455899dda95829
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-06-02 22:50:34 -0400

    incorrect error type

--- rust/routee-compass-codegen/src/generator/constraint/engine.rs
+++ rust/routee-compass-codegen/src/generator/constraint/engine.rs
@@ -1,13 +1,13 @@
 use super::TemplateConfig;
 
-use routee_compass_core::model::traversal::TraversalModelError;
+use routee_compass_core::model::constraint::ConstraintModelError;
 
 pub struct TemplateEngine {
     config: TemplateConfig,
 }
 
 impl TryFrom<TemplateConfig> for TemplateEngine {
-    type Error = TraversalModelError;
+    type Error = ConstraintModelError;
 
     fn try_from(config: TemplateConfig) -> Result<Self, Self::Error> {
         Ok(Self { config })
