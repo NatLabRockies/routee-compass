aa6bf585bdce114ee8449cbcf2638555dd15a942
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-06-02 20:31:04 -0600

    simplify path creation

--- rust/routee-compass-codegen/src/generator/component_type.rs
+++ rust/routee-compass-codegen/src/generator/component_type.rs
@@ -13,39 +13,19 @@ pub enum CodegenComponentType {
 }
 
 impl CodegenComponentType {
-    pub fn template_directory(&self) -> Result<PathBuf, CodegenError> {
-        let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
-        let repo_dir = manifest_dir
-            .parent()
-            .ok_or_else(|| CodegenError::RepoLayout(manifest_dir.to_string_lossy().to_string()))?;
-        match self {
-            CodegenComponentType::Traversal => Ok(repo_dir
-                .join("routee-compass-core")
-                .join("src")
-                .join("model")
-                .join("traversal")
-                .join("template")),
-            CodegenComponentType::Constraint => Ok(repo_dir
-                .join("routee-compass-core")
-                .join("src")
-                .join("model")
-                .join("constraint")
-                .join("template")),
-            CodegenComponentType::InputPlugin => Ok(repo_dir
-                .join("routee-compass")
-                .join("src")
-                .join("plugin")
-                .join("input")
-                .join("template")),
-            CodegenComponentType::OutputPlugin => todo!(),
-        }
+    pub fn template_directory(&self) -> PathBuf {
+        Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("src")
+            .join("generator")
+            .join(self.to_string())
     }
 
     pub fn retrieve_template(&self, file: &str) -> Result<String, CodegenError> {
-        let dir = self.template_directory()?;
+        let dir = self.template_directory();
         let path = dir.join(file);
-        std::fs::read_to_string(path).map_err(|source| CodegenError::TemplateReadError {
+        std::fs::read_to_string(&path).map_err(|source| CodegenError::TemplateReadError {
             component: self.clone(),
+            path: path,
             file: file.to_string(),
             source,
         })
@@ -55,10 +35,10 @@ impl CodegenComponentType {
 impl std::fmt::Display for CodegenComponentType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let s = match self {
-            CodegenComponentType::Traversal => "Traversal",
-            CodegenComponentType::Constraint => "Constraint",
-            CodegenComponentType::InputPlugin => "InputPlugin",
-            CodegenComponentType::OutputPlugin => "OutputPlugin",
+            CodegenComponentType::Traversal => "traversal",
+            CodegenComponentType::Constraint => "constraint",
+            CodegenComponentType::InputPlugin => "input_plugin",
+            CodegenComponentType::OutputPlugin => "output_plugin",
         };
         write!(f, "{s}")
     }
--- rust/routee-compass-codegen/src/generator/error.rs
+++ rust/routee-compass-codegen/src/generator/error.rs
@@ -1,12 +1,15 @@
+use std::path::PathBuf;
+
 use crate::generator::CodegenComponentType;
 
 #[derive(thiserror::Error, Debug)]
 pub enum CodegenError {
     #[error("internal error: path to Cargo manifest {0} has no parent")]
     RepoLayout(String),
-    #[error("error reading {component} {file}: {source}")]
+    #[error("error reading {component} {path} {file}: {source}")]
     TemplateReadError {
         component: CodegenComponentType,
+        path: PathBuf,
         file: String,
         source: std::io::Error,
     },
