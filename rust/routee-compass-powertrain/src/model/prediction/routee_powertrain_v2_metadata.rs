36d97293ee8e3b5b24227291320cb0b37ed9649d
admrtin <adammichaelm.p@gmail.com>
2026-08-26 14:25:49 -0600

    TryFrom to map Feature to InputFeature

--- rust/routee-compass-powertrain/src/model/prediction/routee_powertrain_v2_metadata.rs
+++ rust/routee-compass-powertrain/src/model/prediction/routee_powertrain_v2_metadata.rs
@@ -1,3 +1,7 @@
+use std::str::FromStr;
+
+use routee_compass_core::model::state::InputFeature;
+use routee_compass_core::model::unit::{DistanceUnit, SpeedUnit, TimeUnit};
 /// The minimal set of metadata needed from routee-powertrain v2 to
 /// integrate with Compass's prediction models.
 use serde::{Deserialize, Serialize};
@@ -48,4 +52,33 @@ pub enum EstimatorType {
     ONNXEstimator,
     // If this variant is deserialized, error out. cannot support stochastic models yet.
     NGBoostEstimator,
+}
+
+/// Attempt to convert a `Feature` from the routee-powertrain v2 metadata
+/// into an `InputFeature` used by Compass's prediction models.
+impl TryFrom<&Feature> for InputFeature {
+    type Error = String;
+
+    fn try_from(value: &Feature) -> Result<Self, Self::Error> {
+        let name_lower = value.name.to_lowercase();
+
+        if name_lower.contains("distance") {
+            Ok(InputFeature::Distance {
+                name: value.name.clone(),
+                unit: Some(DistanceUnit::from_str(&value.units)?),
+            })
+        } else if name_lower.contains("speed") {
+            Ok(InputFeature::Speed {
+                name: value.name.clone(),
+                unit: Some(SpeedUnit::from_str(&value.units)?),
+            })
+        } else if name_lower.contains("time") {
+            Ok(InputFeature::Time {
+                name: value.name.clone(),
+                unit: Some(TimeUnit::from_str(&value.units)?),
+            })
+        } else {
+            Err(format!("Input feature {} not supported.", value.name))
+        }
+    }
 }
